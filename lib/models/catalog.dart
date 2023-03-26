11e550c4c74520094dc258c02e75983027e704ff
ezwireless <smabn5@gmail.com>
2023-03-26 11:00:20 +0500

    Catelog dart created

---
+++ lib/models/catalog.dart
@@ -0,0 +1,11 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  // final product = [Item("001", "iphone 12", "apple company", 999, "#33505a", "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-finish-unselect-gallery-1-202207_GEO_US?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1662129048006")]
+}
